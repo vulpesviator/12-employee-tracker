f1ba92c9e873beba0a0dc484cecf0165a355fcb9
travdoeswhat <123843930+vulpesviator@users.noreply.github.com>
2023-06-22 09:42:48 -0500

    Initial commit and SQL schema

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS hr_db;
+
+CREATE DATABASE hr_db;
+
+USE hr_db;
+
+CREATE TABLE department (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT
+);
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT
+);
